b084395ab9bce51c56a991a1efb82a3056874bab
joshgagnuss <joshgagnuss@gmail.com>
2022-04-27 13:37:45 +0800

    edited switch labels and page reload intervals

--- IOT_Assignment_Weather_Controlled_TurfBreeze_Activation/IOT_Assignment_Weather_Controlled_TurfBreeze_Activation.ino
+++ IOT_Assignment_Weather_Controlled_TurfBreeze_Activation/IOT_Assignment_Weather_Controlled_TurfBreeze_Activation.ino
@@ -120,7 +120,7 @@ setInterval(function ( ) {
   };
   xhttp.open("GET", "/temperature", true);
   xhttp.send();
-}, 10000 ) ;
+}, 5000 ) ;
 
 setInterval(function ( ) {
   var xhttp = new XMLHttpRequest();
@@ -131,7 +131,7 @@ setInterval(function ( ) {
   };
   xhttp.open("GET", "/humidity", true);
   xhttp.send();
-}, 10000 ) ;
+}, 5000 ) ;
 
 </script>
 </html>)rawliteral";
@@ -148,7 +148,7 @@ String processor(const String& var) {
     String buttons ="";
     for(int i=1; i<=NUM_RELAYS; i++){
       String relayStateValue = relayState(i);
-      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIO[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
+      buttons+= "<h4>""Manual Override""</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
     }
     return buttons;
   }
